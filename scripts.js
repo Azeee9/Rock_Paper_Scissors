2671c20df4cec3bc79a59dccf8edaea1e1327c1e
Azeee9 <130043358+Azeee9@users.noreply.github.com>
2023-04-26 10:54:43 +0200

    Create a function which confronts player and computer choice

--- scripts.js
+++ scripts.js
@@ -2,12 +2,23 @@ function getComputerChoice () {
     let i = Math.floor((Math.random() * 3) + 1);
     switch(i) {
         case 1:
-            return "rock";
+            return "ROCK";
         case 2:
-            return "paper";
+            return "PAPER";
         case 3:
-            return "scissors";
+            return "SCISSORS";
     }
     }
 
-console.log(getComputerChoice());
+function singleRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {
+        return `Draw! You and computer both chose ${playerSelection}`;
+    } else if(playerSelection == "ROCK" && computerSelection == "SCISSORS" || playerSelection == "PAPER" && computerSelection == "ROCK" || playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+let player = prompt("Choose: Rock, Paper or Scissor").toUpperCase();
+console.log(singleRound(player, getComputerChoice()));
